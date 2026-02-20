123e3d0ae80bec247e44a3a426978480806be2dc
Bryce Jackson <brycejackson@Bryces-MacBook-Air.local>
2026-02-20 03:51:13 -0700

    Security menu

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,55 +1,64 @@
-import { Tabs } from 'expo-router'
-import { View, Text } from 'react-native'
-import {Ionicons} from "@expo/vector-icons"
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
-const TabsLayout = () => {
-    return (
-        <Tabs screenOptions={{
-                tabBarActiveTintColor: "blue",
-                tabBarInactiveTintColor: "black",
-                tabBarStyle:{
-                    backgroundColor: "white",
-                    borderTopWidth: 1,
-                    borderTopColor: "grey",
-                    height: 90,
-                    paddingBottom: 30,
-                    paddingTop: 10,
-                },
-                tabBarLabelStyle: {
-                    fontSize: 12,
-                    fontWeight: "100",
-                },
-                headerShown: false,
-            }}>
-            <Tabs.Screen
-                name='index'
-                options={{
-                    title:"Home",
-                    tabBarIcon: ({color,size}) => (
-                        <Ionicons name='home-outline' size={size} color={color}/>
-                    ),
-                }}
-            />
-            <Tabs.Screen
-                name='about'
-                options={{
-                    title:"About",
-                    tabBarIcon: ({color,size}) => (
-                        <Ionicons name='bar-chart-outline' size={size} color={color}/>
-                    ),
-                }}
-            />
-            <Tabs.Screen
-                name='settings'
-                options={{
-                    title:"Settings",
-                    tabBarIcon: ({color,size}) => (
-                        <Ionicons name='settings' size={size} color={color}/>
-                    )
-                }}
-            />
-        </Tabs>
-    )
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "blue",
+        tabBarInactiveTintColor: "black",
+        tabBarStyle: {
+          backgroundColor: "white",
+          borderTopWidth: 1,
+          borderTopColor: "grey",
+          height: 90,
+          paddingBottom: 30,
+          paddingTop: 10,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "100",
+        },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: "About",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="bar-chart-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Security"
+        options={{
+          title: "Security",
+          tabBarLabel: "Security",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="shield-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
-
-export default TabsLayout
